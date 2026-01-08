82c44daf13fc81bfd6625fae3c76aacda22cb756
Madhan-2004 <138513154+Madhan-2004@users.noreply.github.com>
2026-01-08 15:47:56 +0530

    files updated

--- tests/test_home.py
+++ tests/test_home.py
@@ -3,4 +3,4 @@ from pages.home_page import HomePage
 def test_homepage(driver):
     driver.get("http://localhost:5000")
     home = HomePage(driver)
-    assert "portfolio website" in home.get_text()
+    assert "About the Webpage" in home.get_text()
